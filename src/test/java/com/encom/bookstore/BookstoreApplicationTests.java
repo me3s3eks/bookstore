7a72f1c43a2df8229a728273b2d81699dac4b202
Sergei Petrov <me3s3eks@gmail.com>
2025-03-18 13:23:17 +0300

    struct: create spring boot web app project backbone
    Project structure was created using the spring initializr.

---
+++ src/test/java/com/encom/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encom.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
