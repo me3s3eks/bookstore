68ab6b13655ce49a3015c7fb0973375c7c9e34b9
Sergei Petrov <me3s3eks@gmail.com>
2025-03-27 12:20:15 +0300

    dev: add extra lombok annotations
    This adds @Data and @Builder lombok annotations. Exclude equals and hashCode methods.

--- src/main/java/com/encom/bookstore/model/BookVariantId.java
+++ src/main/java/com/encom/bookstore/model/BookVariantId.java
@@ -1,21 +1,17 @@
 package com.encom.bookstore.model;
 
 import jakarta.persistence.Embeddable;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Embeddable
-@Getter
-@Setter
-@ToString
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookVariantId implements Serializable {
@@ -24,23 +20,4 @@ public class BookVariantId implements Serializable {
 
     private Long bookId;
     private BookType bookType;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof BookVariantId)) return false;
-        BookVariantId other = (BookVariantId) o;
-        if (!other.canEqual(this)) return false;
-        if (!(bookId != null && bookId.equals(other.getBookId()))) return false;
-        return bookType != null && (bookType == other.getBookType());
-    }
-
-    protected boolean canEqual(Object other) {
-        return other instanceof BookVariantId;
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
