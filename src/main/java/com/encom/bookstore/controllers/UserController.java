ad91cc657ac9a9fe89feff77670729efdc0977aa
Sergei Petrov <me3s3eks@gmail.com>
2025-04-14 17:12:48 +0300

    feature: add constraints validation for data from html forms used for create user account and edit user data

--- src/main/java/com/encom/bookstore/controllers/UserController.java
+++ src/main/java/com/encom/bookstore/controllers/UserController.java
@@ -3,8 +3,11 @@ package com.encom.bookstore.controllers;
 import com.encom.bookstore.dto.UserUpdateDTO;
 import com.encom.bookstore.model.User;
 import com.encom.bookstore.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,14 +31,21 @@ public class UserController {
     }
 
     @GetMapping("/edit")
-    public String getUserEditPage() {
+    public String getUserEditPage(Model model) {
+        model.addAttribute("userUpdateDTO", new UserUpdateDTO());
         return "accounts/users/edit";
     }
 
     @PostMapping("/edit")
-    public String updateUser(@ModelAttribute("user") User user, UserUpdateDTO userUpdateDTO) {
-        userService.updateUser(user.getId(), userUpdateDTO);
-        return "redirect:/accounts/users/%d".formatted(user.getId());
+    public String updateUser(@ModelAttribute(value = "user", binding = false) User user,
+                             @Valid @ModelAttribute("userUpdateDTO") UserUpdateDTO userUpdateDTO,
+                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "accounts/users/edit";
+        } else {
+            userService.updateUser(user.getId(), userUpdateDTO);
+            return "redirect:/accounts/users/%d".formatted(user.getId());
+        }
     }
 
     @PostMapping("/delete")
