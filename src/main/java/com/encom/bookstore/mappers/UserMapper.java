3c52b0a5bc5b1b7e8ba2aaf8ec73457ce64d4495
Sergei Petrov <me3s3eks@gmail.com>
2025-05-12 17:04:58 +0300

    feat: add using custom PasswordEncoder for get password hash and store it in db during user account creation

--- src/main/java/com/encom/bookstore/mappers/UserMapper.java
+++ src/main/java/com/encom/bookstore/mappers/UserMapper.java
@@ -5,12 +5,15 @@ import com.encom.bookstore.dto.UserUpdateDto;
 import com.encom.bookstore.model.User;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
+
+    @Mapping(target = "password", ignore = true)
     User userCreateDtoToUser(UserCreateDto userCreateDto);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
