2c51ce5bef3a7758b27dd8e0847f5901f66bab94
Sergei Petrov <me3s3eks@gmail.com>
2025-05-13 11:29:22 +0300

    feat: add functionality for obtaining book category with specify id

--- src/main/java/com/encom/bookstore/controllers/BookCategoriesRestController.java
+++ src/main/java/com/encom/bookstore/controllers/BookCategoriesRestController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,11 @@ public class BookCategoriesRestController {
             bookCategoryPage = bookCategoryService.findAllBookCategoriesByKeyword(pageable, keyword);
         }
         return ResponseEntity.ok(bookCategoryPage);
+    }
+
+    @GetMapping("/{categoryId:\\d+}")
+    public ResponseEntity<BookCategoryDto> getBookCategory(@PathVariable long categoryId) {
+        BookCategoryDto bookCategoryDto = bookCategoryService.findBookCategory(categoryId);
+        return ResponseEntity.ok(bookCategoryDto);
     }
 }
