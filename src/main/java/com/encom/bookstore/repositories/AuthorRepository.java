ff00aa4ff776e7871cd4405424c4163b1f15c8b4
Sergei Petrov <me3s3eks@gmail.com>
2025-05-27 10:04:29 +0300

    refactor: apply specifications for searching Authors entity

--- src/main/java/com/encom/bookstore/repositories/AuthorRepository.java
+++ src/main/java/com/encom/bookstore/repositories/AuthorRepository.java
@@ -1,17 +1,10 @@
 package com.encom.bookstore.repositories;
 
 import com.encom.bookstore.model.Author;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface AuthorRepository extends JpaRepository<Author, Long> {
+public interface AuthorRepository extends JpaRepository<Author, Long>,
+    JpaSpecificationExecutor<Author> {
 
-    @Query("""
-        SELECT a FROM Author a WHERE
-        LOWER(a.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
-        OR LOWER(a.surname) LIKE LOWER(CONCAT('%', :keyword, '%'))
-        """)
-    Page<Author> findAllByKeyword(Pageable pageable, String keyword);
 }
