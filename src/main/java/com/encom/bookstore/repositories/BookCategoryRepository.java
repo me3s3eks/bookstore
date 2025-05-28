ffa07869a1bc494e927025e867859d994394563a
Sergei Petrov <me3s3eks@gmail.com>
2025-05-28 10:32:11 +0300

    refactor: apply specifications for searching BookCategory entity

--- src/main/java/com/encom/bookstore/repositories/BookCategoryRepository.java
+++ src/main/java/com/encom/bookstore/repositories/BookCategoryRepository.java
@@ -1,20 +1,13 @@
 package com.encom.bookstore.repositories;
 
 import com.encom.bookstore.model.BookCategory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
-
-    @Query("""
-        SELECT bc FROM BookCategory bc WHERE
-        LOWER(bc.name) LIKE LOWER(CONCAT('%', :keyword, '%'))
-        """)
-    Page<BookCategory> findAllByKeyword(Pageable pageable, String keyword);
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long>,
+    JpaSpecificationExecutor<BookCategory> {
 
     boolean existsByParentId(long id);
 }
