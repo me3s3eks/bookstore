4877f0d7f61e0c8e819ae0827b888e433d8f4e71
Sergei Petrov <me3s3eks@gmail.com>
2025-05-28 08:42:39 +0300

    merge: PR #11 from refactor/replace-patch-with-put-for-author
    * refactor: replace PATCH with PUT http method to update Author entity

--- src/main/java/com/encom/bookstore/mappers/AuthorMapper.java
+++ src/main/java/com/encom/bookstore/mappers/AuthorMapper.java
@@ -1,17 +1,12 @@
 package com.encom.bookstore.mappers;
 
 import com.encom.bookstore.dto.AuthorBaseInfoDto;
-import com.encom.bookstore.dto.AuthorCreateDto;
+import com.encom.bookstore.dto.AuthorRequestDto;
 import com.encom.bookstore.dto.AuthorDto;
-import com.encom.bookstore.dto.AuthorUpdateDto;
 import com.encom.bookstore.model.Author;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
-
-import java.util.Optional;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AuthorMapper {
@@ -20,12 +15,7 @@ public interface AuthorMapper {
 
     AuthorDto authorToAuthorDto(Author author);
 
-    Author authorCreateDtoToAuthor(AuthorCreateDto authorCreateDto);
+    Author authorRequestDtoToAuthor(AuthorRequestDto authorRequestDto);
 
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    Author updateAuthorFromDto(AuthorUpdateDto authorUpdateDto, @MappingTarget Author author);
-
-    default <T> T mapOptionalToType(Optional<T> optional) {
-        return optional.orElse(null);
-    }
+    Author updateAuthorFromDto(AuthorRequestDto authorRequestDto, @MappingTarget Author author);
 }
