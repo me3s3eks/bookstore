e6add2b5a2df4beea69a3611e8561b4982aea921
Sergei Petrov <me3s3eks@gmail.com>
2025-06-02 16:03:37 +0300

    feat: add methods to Publisher mapper

--- src/main/java/com/encom/bookstore/mappers/PublisherMapper.java
+++ src/main/java/com/encom/bookstore/mappers/PublisherMapper.java
@@ -2,8 +2,10 @@ package com.encom.bookstore.mappers;
 
 import com.encom.bookstore.dto.PublisherBaseInfoDto;
 import com.encom.bookstore.dto.PublisherDto;
+import com.encom.bookstore.dto.PublisherRequestDto;
 import com.encom.bookstore.model.Publisher;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -12,4 +14,8 @@ public interface PublisherMapper {
     PublisherBaseInfoDto publisherToPublisherBaseInfoDto(Publisher publisher);
 
     PublisherDto publisherToPublisherDto(Publisher publisher);
+
+    Publisher publisherRequestDtoToPublisher(PublisherRequestDto publisherRequestDto);
+
+    Publisher updatePublisherFromDto(PublisherRequestDto publisherRequestDto, @MappingTarget Publisher publisher);
 }
