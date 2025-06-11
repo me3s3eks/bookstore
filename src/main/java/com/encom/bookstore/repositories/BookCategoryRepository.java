36757311b914c2917b83a61b0ddc95464f25e334
Sergei Petrov <me3s3eks@gmail.com>
2025-06-11 14:54:31 +0300

    merge: PR #17 from feature/fetch-book-category-id-subtree
    * feat: add method to get all book category IDs in subtree for a given …
    
    * config: set up Lombok to copy the @Lazy annotation from Spring
    
    * feat: add two methods to get all book category IDs in subtree for a g…

--- src/main/java/com/encom/bookstore/repositories/BookCategoryRepository.java
+++ src/main/java/com/encom/bookstore/repositories/BookCategoryRepository.java
@@ -3,11 +3,26 @@ package com.encom.bookstore.repositories;
 import com.encom.bookstore.model.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long>,
     JpaSpecificationExecutor<BookCategory> {
 
     boolean existsByParentId(long id);
+
+    @Query(value = """
+        WITH RECURSIVE category_subtree AS (
+            SELECT id FROM book_categories WHERE id = :rootId
+            UNION
+            SELECT bc.id FROM book_categories bc
+                INNER JOIN category_subtree cs ON bc.parent_id = cs.id
+        )
+        SELECT * FROM category_subtree
+        """, nativeQuery = true)
+    List<Long> findIdsInSubtree(@Param("rootId") long rootId);
 }
--- src/main/java/com/encom/bookstore/services/BookCategoryService.java
+++ src/main/java/com/encom/bookstore/services/BookCategoryService.java
@@ -1,10 +1,12 @@
 package com.encom.bookstore.services;
 
-import com.encom.bookstore.dto.BookCategoryRequestDto;
 import com.encom.bookstore.dto.BookCategoryDto;
+import com.encom.bookstore.dto.BookCategoryRequestDto;
 import com.encom.bookstore.model.BookCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.Set;
 
 public interface BookCategoryService {
 
@@ -15,6 +17,10 @@ public interface BookCategoryService {
     Page<BookCategoryDto> findAllBookCategories(Pageable pageable);
 
     Page<BookCategoryDto> findAllBookCategoriesByKeyword(Pageable pageable, String keyword);
+
+    Set<Long> findBookCategoryIdsInSubtree(long rootId);
+
+    Set<Long> findBookCategoryIdsInSubtree(Set<Long> rootIds);
 
     BookCategory getBookCategory(long categoryId);
 
