c0893af81f774b6fc81dfc1cd943239b51472e4d
Sergei Petrov <me3s3eks@gmail.com>
2025-06-11 17:53:08 +0300

    feat: add ability to find Book IDs with filtering using Specification
    This adds CustomBookRepository interface that declare method to find Book IDs with filtering using Specification.
    
    CustomBookRepository is implemented by CustomBookRepositoryImpl class.
    
    BookRepository interface extends CustomBookRepository.

--- src/main/java/com/encom/bookstore/repositories/BookRepository.java
+++ src/main/java/com/encom/bookstore/repositories/BookRepository.java
@@ -1,25 +1,18 @@
 package com.encom.bookstore.repositories;
 
 import com.encom.bookstore.model.Book;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-    @Query("SELECT b.id FROM Book b")
-    Page<Long> findAllIds(Pageable pageable);
-
-    @Query("SELECT b.id FROM Book b WHERE b.timeOfRemoval IS NULL")
-    Page<Long> findAllActiveIds (Pageable pageable);
+public interface BookRepository extends JpaRepository<Book, Long>,
+    JpaSpecificationExecutor<Book>, CustomBookRepository {
 
     @EntityGraph(attributePaths = {"authors"})
-    List<Book> findAllWithAuthorsByIdIn(List<Long> ids);
+    List<Book> findWithAuthorsByIdIn(List<Long> ids);
 
     @EntityGraph(attributePaths = {"authors", "bookCategory", "publisher"})
     Optional<Book> findWithAllRelatedEntitiesById(long id);
