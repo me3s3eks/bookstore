e551af46c788aa26b46558d37debeb75c96413f2
Sergei Petrov <me3s3eks@gmail.com>
2025-06-11 17:21:43 +0300

    feat: add methods for ISBN validation and normalization

--- src/main/java/com/encom/bookstore/utils/StringUtils.java
+++ src/main/java/com/encom/bookstore/utils/StringUtils.java
@@ -1,11 +1,14 @@
 package com.encom.bookstore.utils;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.mapstruct.Named;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringUtils {
+    private static final int ISBN_MAX_LENGTH = 13;
+
     public static String getBookCategoryFromRequest(HttpServletRequest request) {
         String requestUri = request.getRequestURI();
         String regex = "^/category/(?<category>[^/]+)/books$";
@@ -16,5 +19,17 @@ public class StringUtils {
         } else {
             return "";
         }
+    }
+
+    public static boolean hasValidIsbn(String checkedString) {
+        String regexIsbn = "^(?:97[89][-]?)?(?:\\d{1,5}[-]?\\d{1,7}[-]?\\d{1,7}[-]?[\\dXx]{1})$";
+        Pattern patternIsbn = Pattern.compile(regexIsbn);
+        Matcher matcherIsbn = patternIsbn.matcher(checkedString);
+        return matcherIsbn.find();
+    }
+
+    @Named("normalizeIsbn")
+    public static String normalizeIsbn(String isbn) {
+        return isbn.replaceAll("[^\\dXx]", "").toUpperCase();
     }
 }
