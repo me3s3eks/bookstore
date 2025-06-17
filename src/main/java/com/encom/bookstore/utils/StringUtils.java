dbcc0d7855fc49a062295a68249e9cf849f96955
Sergei Petrov <me3s3eks@gmail.com>
2025-06-17 11:15:16 +0300

    refactor: move pattern compilation to class constants and add ISBN length check
    - moved regex Pattern compilation out of method into class constants;
    - added ISBN max length validation in hasValidation method.

--- src/main/java/com/encom/bookstore/utils/StringUtils.java
+++ src/main/java/com/encom/bookstore/utils/StringUtils.java
@@ -1,35 +1,33 @@
 package com.encom.bookstore.utils;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.mapstruct.Named;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringUtils {
+
     private static final int ISBN_MAX_LENGTH = 13;
 
-    public static String getBookCategoryFromRequest(HttpServletRequest request) {
-        String requestUri = request.getRequestURI();
-        String regex = "^/category/(?<category>[^/]+)/books$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(requestUri);
-        if (matcher.find()) {
-            return matcher.group(1);
-        } else {
-            return "";
-        }
-    }
+    private static final Pattern ISBN_PATTERN = Pattern
+        .compile("^(?:97[89][-]?)?(?:\\d{1,5}[-]?\\d{1,7}[-]?\\d{1,7}[-]?[\\dXx]{1})$");
+
+    private static final Pattern ISBN_DISALLOWED_CHARS_PATTERN = Pattern
+        .compile("[^\\dXx]");
 
     public static boolean hasValidIsbn(String checkedString) {
-        String regexIsbn = "^(?:97[89][-]?)?(?:\\d{1,5}[-]?\\d{1,7}[-]?\\d{1,7}[-]?[\\dXx]{1})$";
-        Pattern patternIsbn = Pattern.compile(regexIsbn);
-        Matcher matcherIsbn = patternIsbn.matcher(checkedString);
-        return matcherIsbn.find();
+        Matcher matcherIsbn = ISBN_PATTERN.matcher(checkedString);
+        if (matcherIsbn.find()) {
+             return normalizeIsbn(checkedString).length() <= ISBN_MAX_LENGTH;
+        }
+        return false;
     }
 
     @Named("normalizeIsbn")
     public static String normalizeIsbn(String isbn) {
-        return isbn.replaceAll("[^\\dXx]", "").toUpperCase();
+        return ISBN_DISALLOWED_CHARS_PATTERN
+            .matcher(isbn)
+            .replaceAll("")
+            .toUpperCase();
     }
 }
