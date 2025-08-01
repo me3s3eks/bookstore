c734dc5b0536d2faa4ae885bd993d109b4f51040
Sergei Petrov <me3s3eks@gmail.com>
2025-08-01 20:05:57 +0300

    feat: add JPA repository for PaperBookProperties entity

---
+++ src/main/java/com/encom/bookstore/repositories/PaperBookPropertiesRepository.java
@@ -0,0 +1,8 @@
+package com.encom.bookstore.repositories;
+
+import com.encom.bookstore.model.PaperBookProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaperBookPropertiesRepository extends JpaRepository<PaperBookProperties, Long> {
+
+}
