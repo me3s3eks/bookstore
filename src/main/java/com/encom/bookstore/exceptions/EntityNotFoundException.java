a534699672dbc391b9534e95a8fe967e42bf9596
Sergei Petrov <me3s3eks@gmail.com>
2025-08-01 20:02:59 +0300

    refactor: expand Set type to Object to enable ability store @EmbeddedId keys

--- src/main/java/com/encom/bookstore/exceptions/EntityNotFoundException.java
+++ src/main/java/com/encom/bookstore/exceptions/EntityNotFoundException.java
@@ -11,9 +11,9 @@ public class EntityNotFoundException extends RuntimeException {
 
     private final String entityName;
 
-    private final Set<Long> entityIds;
+    private final Set<Object> entityIds;
 
-    public EntityNotFoundException(String message, String entityName, Set<Long> entityIds) {
+    public EntityNotFoundException(String message, String entityName, Set<Object> entityIds) {
         super(message);
         this.entityName = entityName;
         this.entityIds = entityIds;
