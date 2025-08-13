77f49fd830a9a04cf893c0fd6da78afc9719865e
Sergei Petrov <me3s3eks@gmail.com>
2025-08-13 16:12:49 +0300

    merge: PR #24 from /feature/find-role-by-name-or-ids
    * feat: add validation error messages for User DTO
    
    * feat: add custom exception for invalid user password case
    
    * feat: add method to find role by name in RoleRepository
    
    * feat: add role lookup by name and by IDs in RoleService

--- src/main/java/com/encom/bookstore/repositories/RoleRepository.java
+++ src/main/java/com/encom/bookstore/repositories/RoleRepository.java
@@ -3,5 +3,9 @@ package com.encom.bookstore.repositories;
 import com.encom.bookstore.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String roleName);
 }
