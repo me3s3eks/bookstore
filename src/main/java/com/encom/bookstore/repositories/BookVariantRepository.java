8b420525519be04c7296d7d229f2c9c83a6fa4d9
Sergei Petrov <me3s3eks@gmail.com>
2025-08-21 18:01:34 +0300

    feat: add BookVariantRepository method to get BookVariant with PESSIMISTIC_WRITE lock

--- src/main/java/com/encom/bookstore/repositories/BookVariantRepository.java
+++ src/main/java/com/encom/bookstore/repositories/BookVariantRepository.java
@@ -3,8 +3,10 @@ package com.encom.bookstore.repositories;
 import com.encom.bookstore.model.Book;
 import com.encom.bookstore.model.BookVariant;
 import com.encom.bookstore.model.BookVariantId;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +19,7 @@ public interface BookVariantRepository extends JpaRepository<BookVariant, BookVa
     @EntityGraph(value = "BookVariant.paperBookProperties")
     List<BookVariant> findWithPropertiesByBook(Book book);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @EntityGraph(value = "BookVariant.paperBookProperties")
-    Optional<BookVariant> findWithPaperBookPropertiesById(BookVariantId bookVariantId);
+    Optional<BookVariant> findLockWithPropertiesById(BookVariantId bookVariantId);
 }
